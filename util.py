41ecb1117f8888b9dc4e234b5d5634245cb643bd
Andrej Uhliarik <uhliarik.a@gmail.com>
2020-11-04 09:20:41 +0100

    Add linlogcut and ensure_shape functions to util.py

--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+import numpy as np
+
+
 def connect(input_layer, layers):
     """ Connect the given sequence of layers and returns output layer
 
@@ -17,3 +21,27 @@ def connect(input_layer, layers):
     for l in layers:
         layer = l(layer)
     return layer
+
+
+def linlogcut(x, a=100, b=1000):
+    """ Function which is linear until a, logarithmic until b and then constant, i.e.
+        y = x               x <= a
+        y = a + log(x-a+1)  a < x < b
+        y = b               b < x
+    """
+    # Cutoff x after b, this should also cutoff infinities
+    x = tf.where(x < b, x, b * tf.ones(tf.shape(x)))
+    return x
+    # Logarithm after a
+    y = a + tf.where(x < a, x - a, tf.math.log(x - a + 1))
+    # Make sure everything is finite
+    y = tf.where(tf.math.is_finite(y), y, b * tf.ones(tf.shape(y)))
+    return y
+
+
+def ensure_shape(X):
+    if np.ndim(X) == 2:
+        return X
+    if np.ndim(X) == 1:
+        return np.array([X])
+    raise ValueError('Incompatible array with shape: ', np.shape(X))
