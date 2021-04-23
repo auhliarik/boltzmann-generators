38b468b05b79688b5b683fee9005f06c30f6ec7d
Andrej Uhliarik <uhliarik.a@gmail.com>
2021-04-23 18:30:52 +0200

    Change the meaning of parameter 'n_layers' in the nonlinear_transform
    Up to now its meaning was the total number of layers,
    i.e. including the output layer. From now, its meaning will
    be the number of hidden layers.

--- layers_basic.py
+++ layers_basic.py
@@ -6,36 +6,39 @@ import copy
 
 def nonlinear_transform(output_size, n_layers=3, n_hidden=100, activation='relu',
                         init_outputs=None, **args):
-    """ Generic dense trainable neural nonlinear transform
-
-    Returns a list of the layers of a dense feedforward network with
-    n_layers-1 hidden layers with n_hidden neurons and the specified activation
-    functions. The last layer is always linear in order to access the full real
-    number range and has output_size output neurons.
+    """ Returns layers for a generic dense trainable neural nonlinear transform
 
     Arguments:
         output_size (int):
             Number of output neurons.
         n_layers (int):
-            Number of layers, including the linear output layer. n_layers=3 means two
-            hidden layers with nonlinear activation and one linear output layer.
-        n_hidden (int):
-            Number of neurons in each hidden layer, either a number or an array of
-            length n_layers-1 to specify the width of each hidden layer.
+            Number of hidden layers of the dense NN.
+        n_hidden (int or list):
+            Number of neurons (units) in each hidden layer, either a number
+            or an array of length n_layers to specify the width of individual hidden layers.
         activation (str):
-            Nonlinear activation function in hidden layers.
+            Nonlinear activation function used in the hidden layers.
         init_outputs (None or float or array):
             None means default initialization for the output layer, otherwise
             it is initialized with vector of given constants.
         args (kwargs):
             Additional keyword arguments passed to the layer.
+
+    Returns:
+        list[tf.keras.layers.Dense]:
+            List of the layers for a dense feedforward neural network that consists of
+            - n_layers hidden layers with n_hidden neurons using the specified
+              activation function and
+            - the last layer which is always linear (without the act. func.)
+              in order to access the full range of the real numbers
+              and has output_size output neurons.
     """
     if isinstance(n_hidden, numbers.Integral):
-        n_hidden = n_hidden * np.ones(n_layers-1, dtype=int)
+        n_hidden = n_hidden * np.ones(n_layers, dtype=int)
     else:
         n_hidden = np.array(n_hidden)
-        if n_hidden.size != n_layers-1:
-            raise ValueError('Illegal size of n_hidden. Expecting 1d array with n_layers-1 elements')
+        if n_hidden.size != n_layers:
+            raise ValueError("Illegal size of n_hidden. Expecting 1D array with n_layers elements")
 
     neural_network_layers = [tf.keras.layers.Dense(nh, activation=activation, **args) for nh in n_hidden]
 
@@ -57,7 +60,7 @@ class IndexLayer(tf.keras.layers.Layer):
         self.indices = indices
         super().__init__(**kwargs)
 
-    def call(self, x):
+    def call(self, x, **kwargs):
         # Gathering part of an input defined by self.indices
         return tf.gather(x, self.indices, axis=1)
 
